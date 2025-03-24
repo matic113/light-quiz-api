bb3e4c23a4efd734db2dd373bbd13b3c40c98a75
matic113 <faressafi203@gmail.com>
2025-03-24 23:51:05 +0200

    DB: added a migrations to seed roles

---
+++ Migrations/20250324212330_SeedRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace light_quiz_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "id", "name", "normalized_name", "concurrency_stamp" },
+                values: new object[] { Guid.NewGuid(), "student", "STUDENT", Guid.NewGuid().ToString() });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "id", "name", "normalized_name", "concurrency_stamp" },
+                values: new object[] { Guid.NewGuid(), "teacher", "TEACHER", Guid.NewGuid().ToString() });
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"public.AspNetRoles\" WHERE \"name\" = 'student'");
+            migrationBuilder.Sql("DELETE FROM \"public.AspNetRoles\" WHERE \"name\" = 'teacher'");
+        }
+    }
+}
