62f19876437e83ccc7ba0bc1cb6088632e2cca81
matic113 <faressafi203@gmail.com>
2025-04-14 21:48:53 +0200

    changed sub to show FullName

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -115,7 +115,7 @@ public class AuthService : IAuthService
         var claims = new[]
         {
             new Claim("userId", userId),
-            new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Sub, user.FullName!),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(JwtRegisteredClaimNames.Email, user.Email!)
         }
