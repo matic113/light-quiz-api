fcf31460b07ebfbc543f06051abe8988145fa6d0
matic113 <faressafi203@gmail.com>
2025-04-17 01:53:40 +0200

    Added Serilog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 using light_quiz_api;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 
 builder.AddServices();
 
 var app = builder.Build();
 
 await app.Configure();
+
+//Add support to logging request with SERILOG
+app.UseSerilogRequestLogging();
+
 
 app.MapGet("/health", () => "Hello World!")
     .WithTags("Health");
