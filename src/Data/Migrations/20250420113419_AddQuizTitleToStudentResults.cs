8991aa4f8159e1e30c6c2d832840ee0baa552635
matic113 <faressafi203@gmail.com>
2025-04-20 13:34:38 +0200

    FIX: add migration to add quiz title to student results

---
+++ src/Data/Migrations/20250420113419_AddQuizTitleToStudentResults.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace light_quiz_api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizTitleToStudentResults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "quiz_title",
+                table: "user_results",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "quiz_title",
+                table: "user_results");
+        }
+    }
+}
